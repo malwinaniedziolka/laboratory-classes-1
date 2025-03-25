aaff702185e73cabb3068ce51eb531f7bf764486
MALWINA_NIEDZIOLKA <mmalwina.niedziolka@gmail.com>
2025-03-25 17:20:21 +0100

    server and config updated

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
 // 📦 Musisz zaimportować tutaj moduł 'http'.
+const http = require("http");
+const config = require('./config.js')
+
 // 📦 Żeby użyć tutaj PORT, musisz zaimportować go z modułu konfiguracyjnego z pliku 'config.js'.
+const server = http.createServer((request, response) => {
+
+});
+
+server.listen(config.PORT, () => {
+    console.log("Server working on http://localhost:3000")
+});
+
 // 📦 Zaimportuj funkcję 'requestRouting' z modułu 'routing/routing.js'.
 
 // 🏗 Tutaj, stwórz funkcję 'requestListener, która przekazuje 'request' i 'response' do 'requestRouting'.
