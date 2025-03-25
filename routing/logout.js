6bc16b7faecc6cfbfcaa3fd6640fc42eff1f4e60
MALWINA_NIEDZIOLKA <mmalwina.niedziolka@gmail.com>
2025-03-25 18:17:15 +0100

    logout.js done

--- routing/logout.js
+++ routing/logout.js
@@ -2,8 +2,13 @@ const logoutRouting = (method, response) => {
     response.setHeader("Content-Type", "text/html");
     response.write("<html>");
     response.write("<head><title>Shop – Logout</title></head>");
-    response.write("<body><h1>Logout</h1></body>");
-    //nav i 2 a??? wut
+    response.write("<body>");
+    response.write("<h1>Logout</h1>");
+    response.write("<nav>");
+    response.write("<a href="/">Home</a>"); 
+    response.write("<a href="/kill,">Logout from application</a>");
+    response.write("</nav>");
+    response.write("</body>");
     response.write("</html>");
     return response.end();
 }
